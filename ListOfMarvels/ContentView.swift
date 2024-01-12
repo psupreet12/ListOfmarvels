38ec116d5a1542bbd2d3925e67607f5d5f300bdd
Supreet <psupreet12@gmail.com>
2024-01-12 15:03:30 +0530

    Initial Commit

---
+++ ListOfMarvels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListOfMarvels
+//
+//  Created by Supreet on 12/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListOfMarvels/ListOfMarvelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfMarvelsApp.swift
+//  ListOfMarvels
+//
+//  Created by Supreet on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ListOfMarvelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
